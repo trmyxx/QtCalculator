d4b97dca66bb7b71e3d5d89575479111bdbd8d21
trmyx <kirqllg@gmail.com>
2025-07-27 16:15:59 +0300

    first

---
+++ Calculator.cpp
@@ -0,0 +1,225 @@
+#include "Calculator.h"
+#include "ui_Calculator.h"
+#include <cmath>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+int num1;
+char act;
+bool f=false;
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(1));
+    a=a*10+1;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(2));
+    a=a*10+2;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(3));
+    a=a*10+3;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(4));
+    a=a*10+4;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(5));
+    a=a*10+5;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(6));
+    a=a*10+6;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(7));
+    a=a*10+7;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(8));
+    a=a*10+8;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    if(a==0) ui->lineEdit_res->setText(QString::number(9));
+    a=a*10+9;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+
+void MainWindow::on_pushButton_0_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    a=a*10;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+void MainWindow::on_pushButton_plus_clicked()
+{
+    if(f==false){
+        act='+';
+        num1=ui->lineEdit_res->text().toInt();
+        ui->lineEdit_res->clear();
+    }
+    f=true;
+}
+
+
+void MainWindow::on_pushButton_minus_clicked()
+{
+    if(f==false){
+        act='-';
+        num1=ui->lineEdit_res->text().toInt();
+        ui->lineEdit_res->clear();
+    }
+    f=true;
+}
+
+
+void MainWindow::on_pushButton_mutliply_clicked()
+{
+    if(f==false){
+        act='*';
+        num1=ui->lineEdit_res->text().toInt();
+        ui->lineEdit_res->clear();
+    }
+    f=true;
+}
+
+
+void MainWindow::on_pushButton_15_clicked()
+{
+    if(f==false){
+        act='/';
+        num1=ui->lineEdit_res->text().toInt();
+        ui->lineEdit_res->clear();
+    }
+    f=true;
+}
+
+
+void MainWindow::on_pushButton_equal_clicked()
+{
+    long long int num2=ui->lineEdit_res->text().toInt();
+    long long int res = 0;
+    if(act=='+'){
+        res=num1+num2;
+    }
+    else if(act=='-'){
+        res=num1-num2;
+    }
+    else if(act=='*'){
+        res=num1*num2;
+    }
+    else if(act=='/'){
+        if(num2 == 0) {
+            QMessageBox::critical(nullptr, "ERROR", "You can't divide by 0");
+            ui->lineEdit_res->clear();
+            return;
+        }
+        res=num1/num2;
+    }
+    else if(act=='^'){
+        res=pow(num1, num2);
+    }
+    else {
+        QMessageBox::warning(this, "ERROR", "Unknown operation.");
+        ui->lineEdit_res->clear();
+        return;
+    }
+
+    ui->lineEdit_res->setText(QString::number(res));
+    f=false;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->lineEdit_res->clear();
+}
+
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    a=a/10;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+void MainWindow::on_pushButton_root_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    a=sqrt(a);
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
+void MainWindow::on_pushButton_power_clicked()
+{
+    if(f==false){
+        act='^';
+        num1=ui->lineEdit_res->text().toInt();
+        ui->lineEdit_res->clear();
+    }
+    f=true;
+}
+
+void MainWindow::on_pushButton_pi_clicked()
+{
+    long long int a = ui->lineEdit_res->text().toInt();
+    a*=3.14;
+    ui->lineEdit_res->setText(QString::number(a));
+}
+
---
+++ Calculator/build/Desktop_Qt_6_9_0_llvm_mingw_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,207 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QLineEdit *lineEdit_res;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton_7;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_mutliply;
+    QPushButton *pushButton_minus;
+    QPushButton *pushButton_1;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_plus;
+    QPushButton *pushButton_15;
+    QPushButton *pushButton_equal;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_0;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(328, 386);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName("horizontalLayout");
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName("verticalLayout_2");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        lineEdit_res = new QLineEdit(centralwidget);
+        lineEdit_res->setObjectName("lineEdit_res");
+
+        verticalLayout->addWidget(lineEdit_res);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName("gridLayout");
+        pushButton_7 = new QPushButton(centralwidget);
+        pushButton_7->setObjectName("pushButton_7");
+
+        gridLayout->addWidget(pushButton_7, 2, 0, 1, 1);
+
+        pushButton_8 = new QPushButton(centralwidget);
+        pushButton_8->setObjectName("pushButton_8");
+
+        gridLayout->addWidget(pushButton_8, 2, 1, 1, 1);
+
+        pushButton_mutliply = new QPushButton(centralwidget);
+        pushButton_mutliply->setObjectName("pushButton_mutliply");
+
+        gridLayout->addWidget(pushButton_mutliply, 4, 1, 1, 1);
+
+        pushButton_minus = new QPushButton(centralwidget);
+        pushButton_minus->setObjectName("pushButton_minus");
+
+        gridLayout->addWidget(pushButton_minus, 4, 0, 1, 1);
+
+        pushButton_1 = new QPushButton(centralwidget);
+        pushButton_1->setObjectName("pushButton_1");
+
+        gridLayout->addWidget(pushButton_1, 0, 0, 1, 1);
+
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+
+        gridLayout->addWidget(pushButton_4, 1, 0, 1, 1);
+
+        pushButton_plus = new QPushButton(centralwidget);
+        pushButton_plus->setObjectName("pushButton_plus");
+
+        gridLayout->addWidget(pushButton_plus, 3, 0, 1, 1);
+
+        pushButton_15 = new QPushButton(centralwidget);
+        pushButton_15->setObjectName("pushButton_15");
+
+        gridLayout->addWidget(pushButton_15, 4, 2, 1, 1);
+
+        pushButton_equal = new QPushButton(centralwidget);
+        pushButton_equal->setObjectName("pushButton_equal");
+
+        gridLayout->addWidget(pushButton_equal, 3, 2, 1, 1);
+
+        pushButton_9 = new QPushButton(centralwidget);
+        pushButton_9->setObjectName("pushButton_9");
+
+        gridLayout->addWidget(pushButton_9, 2, 2, 1, 1);
+
+        pushButton_0 = new QPushButton(centralwidget);
+        pushButton_0->setObjectName("pushButton_0");
+
+        gridLayout->addWidget(pushButton_0, 3, 1, 1, 1);
+
+        pushButton_5 = new QPushButton(centralwidget);
+        pushButton_5->setObjectName("pushButton_5");
+
+        gridLayout->addWidget(pushButton_5, 1, 1, 1, 1);
+
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+
+        gridLayout->addWidget(pushButton_2, 0, 1, 1, 1);
+
+        pushButton_6 = new QPushButton(centralwidget);
+        pushButton_6->setObjectName("pushButton_6");
+
+        gridLayout->addWidget(pushButton_6, 1, 2, 1, 1);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+
+        gridLayout->addWidget(pushButton_3, 0, 2, 1, 1);
+
+
+        verticalLayout->addLayout(gridLayout);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        pushButton_10 = new QPushButton(centralwidget);
+        pushButton_10->setObjectName("pushButton_10");
+
+        verticalLayout_2->addWidget(pushButton_10);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+
+        verticalLayout_2->addWidget(pushButton);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        MainWindow->setCentralWidget(centralwidget);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "7", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "8", nullptr));
+        pushButton_mutliply->setText(QCoreApplication::translate("MainWindow", "*", nullptr));
+        pushButton_minus->setText(QCoreApplication::translate("MainWindow", "-", nullptr));
+        pushButton_1->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "4", nullptr));
+        pushButton_plus->setText(QCoreApplication::translate("MainWindow", "+", nullptr));
+        pushButton_15->setText(QCoreApplication::translate("MainWindow", "/", nullptr));
+        pushButton_equal->setText(QCoreApplication::translate("MainWindow", "=", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "9", nullptr));
+        pushButton_0->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "5", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "2", nullptr));
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "6", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "3", nullptr));
+        pushButton_10->setText(QCoreApplication::translate("MainWindow", "<--", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Clear all", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
